acbf2cfe7159f3a1796f10d783ea51f683022de9
baracudaz <baracudaz@users.noreply.github.com>
2026-07-17 09:10:18 +0200

    Fix deploy script to ensure service starts if not running

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,7 @@ git fetch origin
 if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
   git reset --hard origin/main
   docker compose up --build -d
+elif [ -z "$(docker ps -q -f name=^/bazosbot$ -f status=running)" ]; then
+  echo "bazosbot is not running. Starting it..."
+  docker compose up -d
 fi
